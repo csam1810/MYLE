839711c5cfe17f285bb55ff3ddbf66583064b3fa
csam1810 <alexandra.jaeger@student.uibk.ac.at>
2015-07-01 08:17:25 +0200

    DC: update Createaccount phpunit test

--- module/Createaccount/test/CreateaccountTest/Model/CreateaccountTableTest.php
+++ module/Createaccount/test/CreateaccountTest/Model/CreateaccountTableTest.php
@@ -6,6 +6,8 @@ use PHPUnit_Framework_TestCase;
 use Createaccount\Model\CreateaccountTable;
 use Createaccount\Model\Createaccount;
 
+include 'config/autoload/functions.php';
+
 class CreateaccountTableTest extends PHPUnit_Framework_TestCase {
 
     //create new account
@@ -18,8 +20,47 @@ class CreateaccountTableTest extends PHPUnit_Framework_TestCase {
             'displayName' => 'newUser',
             'phoneNo' => '098767899876',
             'password' => 123,
+            'repassword' => 123,
         );
-
+      $rs = getResultSet("User");
+        foreach ($rs as $row) {
+                      if(strcmp($data['userID'] ,(string)$row['userID'])==0){
+                            echo "<script>alert('Error: User id dose exist!');</script>";
+                            goto out;
+                      }
+                 }
+                 if(strcmp($data['password'] ,$data['repassword'] )==0){
+                    $str= "INSERT INTO User(userID, displayName, phoneNo, password) VALUES ('";
+                    executeQuery($str . $data['userID'] . "','" . $data['displayName'] . "','" . $data['phoneNo'] . "','" . $data['password'] . "')");
+                    echo "<script>alert('User successfully created!');</script>";
+                    login($data['userID']);
+                 }
+                 else{
+                     echo "<script>alert('Error: Password and Re-Password do not match!');</script>";
+                 }
+        out:
+            
+    /*    $rs = getResultSet("User");
+        foreach ($rs as $row) {
+                      if(strcmp($data->userID ,(string)$row['userID'])==0){
+                            echo "<script>alert('Error: User id dose exist!');</script>";
+                            goto out;
+                      }
+                 }
+                 if(strcmp($data->password ,$data->repassword )==0){
+                    $str= "INSERT INTO User(userID, displayName, phoneNo, password) VALUES ('";
+                    executeQuery($str . $data->userID . "','" . $data->displayName . "','" . $data->phoneNo . "','" . $data->password . "')");
+                    echo "<script>alert('User successfully created!');</script>";
+                    login($data->userID);
+                    return $this->redirect()->toRoute('recipe', array('action' => 'index'));
+                 }
+                 else{
+                     echo "<script>alert('Error: Password and Re-Password do not match!');</script>";
+                 }
+        out:
+     * 
+     */
+   /*
         $user = new Createaccount();
         $user->exchangeArray($data);     
         
@@ -32,7 +73,8 @@ class CreateaccountTableTest extends PHPUnit_Framework_TestCase {
 
         $userTable = new CreateaccountTable($mockTableGateway);
         $userTable->saveCreateAccount($user);
-
+    * 
+    */
 
       /*
         $userRowData = $this->getUser($data->userID);
